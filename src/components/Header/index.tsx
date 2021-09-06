ac88a88e4ecf42f04e622923c609c62c8574e298
Andrio <andrio.rodrigo.silva@hotmail.com>
2021-09-06 15:52:08 -0300

    changes

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header:React.FC = () => {
+    const person = {
+        first: "name", };
+    return (
+        <>
+            <h1>Header</h1>
+        </>
+    )
+}
+
+export default Header
